cf6979d352f8d6c05988a84747a59606ab38acf8
Siddharth Ajmera <siddajmera@outlook.com>
2026-04-07 10:11:04 +0000

    bugfix(setup): fix an issue with the .env file generating wrong values for SA

--- setup/setup_env.sh
+++ setup/setup_env.sh
@@ -18,9 +18,6 @@ gcloud services enable apikeys.googleapis.com --project=$PROJECT_ID
 # gcloud services enable mapstools.googleapis.com --project=$PROJECT_ID
 gcloud services enable bigquery.googleapis.com --project=$PROJECT_ID
 ENABLED_SERVICES=$(gcloud beta services mcp list --enabled --format="value(name.basename())" --project=$PROJECT_ID)
-# if [[ ! "$ENABLED_SERVICES" == *"mapstools.googleapis.com"* ]]; then
-#     gcloud --quiet beta services mcp enable mapstools.googleapis.com --project=$PROJECT_ID
-# fi
 if [[ ! "$ENABLED_SERVICES" == *"bigquery.googleapis.com"* ]]; then
     gcloud --quiet beta services mcp enable bigquery.googleapis.com --project=$PROJECT_ID
 fi
@@ -30,6 +27,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ENV_FILE="$SCRIPT_DIR/../agents/mcp_medical_app/.env"
 mkdir -p $(dirname "$ENV_FILE")
 
+SA_NAME="hackathon-vena"
+SA_EMAIL="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
+
 cat <<EOF > "$ENV_FILE"
 # Google Cloud
 GOOGLE_GENAI_USE_VERTEXAI=1
@@ -37,8 +37,8 @@ GOOGLE_CLOUD_PROJECT=$PROJECT_ID
 GOOGLE_CLOUD_LOCATION=global
 GOOGLE_GEMINI_MODEL="gemini-2.5-flash"
 BIG_QUERY_DATASET="mcp_medical"
-GOOGLE_CLOUD_SA_NAME=hackathon-vena
-GOOGLE_CLOUD_SERVICE_ACCOUNT=${GOOGLE_CLOUD_SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com
+GOOGLE_CLOUD_SA_NAME=$SA_NAME
+GOOGLE_CLOUD_SERVICE_ACCOUNT=$SA_EMAIL
 
 # NCBI / PubMed
 NCBI_API_KEY=<YOUR_NCBI_API_KEY>
